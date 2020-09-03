cc03b7f071394b190e1890260a9edf22c96bcb3e
zokicpokic <zoran@diffine.com>
2020-09-03 16:37:09 +0200

    changing main.js

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,30 @@ import store from './store';
 import VueRouter from 'vue-router';
 import router from './routes.js';
 import vuetify from './plugins/vuetify';
+import constants from '@/utils/constants'
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-  vuetify,
-  router,
+const getRuntimeConfig = async () => {
+  const runtimeConfig = await fetch('static/configuration.json');
+  return await runtimeConfig.json()
+}
+
+getRuntimeConfig().then(function(json) {
+  console.log("Runtime config: " + JSON.stringify(json));
+  for (let i in constants) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (json.hasOwnProperty(i)) {
+      constants[i] = json[i];
+    }
+  }
+  Object.freeze(constants);
+
+  new Vue({
+    render: h => h(App),
+    vuetify,
+    router,
     store
-}).$mount('#app')
+  }).$mount('#app')
+});
